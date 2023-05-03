dfeb602139de738bb878613a9f54094667e49e8b
edoardogallinelli <e.gallinelli@almaviva.it>
2023-05-03 18:51:16 +0200

    service di login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import {FormBuilder, Validators} from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/services/user-service.service';
 
 @Component({
   selector: 'app-login',
@@ -7,13 +9,32 @@ import {FormBuilder, Validators} from '@angular/forms';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  firstFormGroup = this._formBuilder.group({
-    firstCtrl: ['', Validators.required],
-  });
-  secondFormGroup = this._formBuilder.group({
-    secondCtrl: ['', Validators.required],
+  public formAccesso = new FormGroup({
+    mail: new FormControl(),
+    password: new FormControl()
   });
   isLinear = false;
 
-  constructor(private _formBuilder: FormBuilder) {}
+  constructor(private _formBuilder: FormBuilder, private userService: UserService, private router : Router) { }
+
+  ngOnInit(){
+    if (this.userService.getUser()) {
+      this.router.navigateByUrl("area-utente")
+    }
+  }
+  private mail : any;
+  private password : any;
+  private utenteId : any;
+
+  async login(){
+    this.mail = this.formAccesso.value.mail;
+    this.password = this.formAccesso.value.password;
+    await this.userService.login(this.mail, this.password);
+    debugger
+    if (this.userService.getUser()) {
+      this.router.navigateByUrl('area-utente')
+    } else {
+      alert("Autenticazione fallita")
+    }
+  }
 }
