ed014dc3010e341169f3835bcdebf4143d961d5d
EleRoss1 <eleonora.rossetti05@libero.it>
2023-05-03 18:57:28 +0200

    modifica panoramica-focus

--- src/app/components/panoramica-focus/panoramica-focus.component.ts
+++ src/app/components/panoramica-focus/panoramica-focus.component.ts
@@ -6,5 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./panoramica-focus.component.css']
 })
 export class PanoramicaFocusComponent {
+  data: any;
+
+  options: any;
+
+  ngOnInit() {
+
+      this.data = {
+          labels: ['A', 'B', 'C'],
+          datasets: [
+              {
+                  data: [300, 50, 100],
+                  backgroundColor: ["#f00","#0f0","#00f"],
+                  hoverBackgroundColor: ["#000","#000","#000"]
+              }
+          ]
+      };
+
+
+      this.options = {
+          cutout: '60%',
+          plugins: {
+              legend: {
+                  labels: {
+                      color: "black"
+                  }
+              }
+          }
+      };
+  }
 
 }
