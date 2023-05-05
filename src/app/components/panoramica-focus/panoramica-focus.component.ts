b3051869f8cc268bfbc8544850089855eb60ed29
EleRoss1 <eleonora.rossetti05@libero.it>
2023-05-05 09:08:00 +0200

    modifiche panoramica-focus

--- src/app/components/panoramica-focus/panoramica-focus.component.ts
+++ src/app/components/panoramica-focus/panoramica-focus.component.ts
@@ -1,24 +1,24 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-panoramica-focus',
-  templateUrl: './panoramica-focus.component.html',
-  styleUrls: ['./panoramica-focus.component.css']
+    selector: 'app-panoramica-focus',
+    templateUrl: './panoramica-focus.component.html',
+    styleUrls: ['./panoramica-focus.component.css']
 })
 export class PanoramicaFocusComponent {
     data: any;
-    costo: any[]=["50"];
+    costo: string = "50";
     options: any;
-    percentuale: any[]=["100"];
+    percentuale: string = "80";
     ngOnInit() {
         this.data = {
             labels: ['Rete propria', 'Rete della comunità', 'Rete nazionale'],
             datasets: [
-            {
-                data: [300, 50, 100],
-                backgroundColor: ["#18A6E0","#FFC701","#2ECC71"],
-                hoverBackgroundColor: ["#000","#000","#000"]
-            }
+                {
+                    data: [300, 50, 100],
+                    backgroundColor: ["#18A6E0", "#FFC701", "#2ECC71"],
+                    hoverBackgroundColor: ["#000", "#000", "#000"]
+                }
             ]
         };
         this.options = {
@@ -34,7 +34,7 @@ export class PanoramicaFocusComponent {
         };
     }
 
-    
+
 }
 
 
