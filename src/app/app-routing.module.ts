3156adf2de27aa7731c21e9418d4f0856e7deeb6
edoardogallinelli <e.gallinelli@almaviva.it>
2023-05-09 17:26:27 +0200

    fix scroll to fragment

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,10 +12,16 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent},
   { path: 'registration', component: RegistrationComponent},
   { path: 'area-utente', component: AreaUtenteComponent},
+  //fallback, qualsiasi path che non matcha con i precedenti riporta in home
+  { path: '**', component: HomeComponent},
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    anchorScrolling: 'enabled',
+    onSameUrlNavigation: 'reload',
+    // scrollPositionRestoration: 'enabled'
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
