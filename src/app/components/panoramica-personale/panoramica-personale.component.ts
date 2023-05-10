77152a67b47206490913b6b5bf51662b06bad1a0
edoardogallinelli <e.gallinelli@almaviva.it>
2023-05-10 10:17:52 +0200

    prosumer/consumer

--- src/app/components/panoramica-personale/panoramica-personale.component.ts
+++ src/app/components/panoramica-personale/panoramica-personale.component.ts
@@ -1,6 +1,7 @@
 import { style } from '@angular/animations';
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { UserService } from 'src/app/services/user-service.service';
 
 
 @Component({
@@ -10,12 +11,15 @@ import { Component } from '@angular/core';
 })
 export class PanoramicaPersonaleComponent {
   data: any;
+  dataConsumer: any;
   options: any;
   urlIconaMeteo: string = "";
-  efficienzaSolare: string= "";
-
+  efficienzaSolare: string = "";
+  efficienzaSolareConsumer: string = "";
+  public isProsumer: boolean = true;
   constructor(
-    private http: HttpClient
+    private http: HttpClient,
+    private userService: UserService
   ) {
 
   }
@@ -23,7 +27,7 @@ export class PanoramicaPersonaleComponent {
 
 
   ngOnInit() {
-
+    this.isProsumer = this.userService.getUser().prosumer
     this.http.get("http://localhost:3000/meteo?localita=rome")
       .subscribe((response: any) => {
         //qui dentro definiamo quello che succede quando la chiamata `e terminata
@@ -31,13 +35,16 @@ export class PanoramicaPersonaleComponent {
 
         if (response[0].meteo == "soleggiato") {
           this.urlIconaMeteo = "../../../assets/images/soleggiato.png"
-          this.efficienzaSolare= " Il tuo pannello ha un'efficienza del 25%"
+          this.efficienzaSolare = " Il tuo pannello adesso ha un'efficienza del 25%"
+          this.efficienzaSolareConsumer = "La comunità adesso è al 25% dell'efficienza"
         } else if (response[0].meteo == "nuvoloso") {
           this.urlIconaMeteo = "../assets/images/nuvoloso.png"
-          this.efficienzaSolare="Il tuo pannello ha un'efficenza del 15%"
+          this.efficienzaSolare = "Il tuo pannello adesso ha un'efficenza del 15%"
+          this.efficienzaSolareConsumer = "La comunità adesso è al 15% dell'efficienza"
         } else if (response[0].meteo == "piovoso") {
           this.urlIconaMeteo = "../src/assets/images/piovoso.png"
-          this.efficienzaSolare="Il tuo pannello ha un'efficenza del 10%"
+          this.efficienzaSolare = "Il tuo pannello adesso ha un'efficenza del 10%"
+          this.efficienzaSolareConsumer = "La comunità adesso è al 10% dell'efficienza"
         }
       });
 
@@ -76,6 +83,29 @@ export class PanoramicaPersonaleComponent {
       ]
     };
 
+
+    this.dataConsumer = {
+      labels: ['Lun', 'Mar', 'Mer', 'Gio', 'Ven', 'Sab', 'Dom'],
+      datasets: [
+        {
+          label: 'Energia da comunità',
+          data: [15, 11, 25, 3, 7, 13, 11],
+          fill: false,
+          borderColor: ' #ffc40c',
+          tension: 0.4,
+
+        },
+        {
+          label: 'Energia da rete nazionale',
+          data: [5, 9, 5, 7, 8, 4, 11],
+          fill: true,
+          borderDash: [5, 5],
+          borderColor: '#ace1af',
+          tension: 0.4,
+          backgroundColor: "#ace1af"
+        },
+      ]
+    };
     this.options = {
       responsive: false,
       maintainAspectRatio: false,
